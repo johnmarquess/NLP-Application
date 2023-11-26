b562ccb6501332036356ae47e9132e05ead6592e
John Marquess <john@marquess.me>
2023-11-26 20:10:36 +1000

    Improve file upload and data management features
    This commit updates the file upload method, combining GET and POST in '/upload' for simplicity. This enables the '/upload' route to display the upload form and process the upload simultaneously. A new route '/data_management' was created to enhance user interaction with uploaded data, adding features to list, select, and process these files.
    
    This update also includes the addition of data management and data modeling pages and adjustments to the index.html and upload.html files to reflect these feature enhancements. Configurations improved and csv-editor.xml was added under .idea directory for better management of database CSV files. Furthermore, a navbar was incorporated into base.html for seamless app navigation.
    
    Overall, these changes enhance the app's usability, offering a streamlined workflow from data upload to data management and model execution.

--- app/routes.py
+++ app/routes.py
@@ -7,40 +7,68 @@ from app import app
 
 
 def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+    return (
+            "." in filename
+            and filename.rsplit(".", 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
+    )
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route("/upload", methods=["GET", "POST"])
 def upload_file():
-    if request.method == 'POST':
-        if 'datafile' not in request.files:
-            flash('No file part', 'error')
+    if request.method == "POST":
+        if "datafile" not in request.files:
+            flash("No file part", "error")
             return redirect(request.url)
-        file = request.files['datafile']
-        if file.filename == '':
-            flash('No selected file', 'error')
+        file = request.files["datafile"]
+        if file.filename == "":
+            flash("No selected file", "error")
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             # Change this to save in the DATA_RAW_FOLDER
+            file.save(os.path.join(app.config["DATA_RAW_FOLDER"], filename))
+            flash(
+                f'File uploaded successfully to {app.config["DATA_RAW_FOLDER"]}',
+                "success",
+            )
+            return redirect(
+                url_for(url_for('data_management'))
+            )  # Or any other route you'd like to redirect to
+    return render_template("upload.html")
+
+
+@app.route('/data_management', methods=['GET', 'POST'])
+def data_management():
+    if request.method == 'POST':
+        file = request.files.get('datafile')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['DATA_RAW_FOLDER'], filename))
             flash(f'File uploaded successfully to {app.config["DATA_RAW_FOLDER"]}', 'success')
-            return redirect(url_for('index'))  # Or any other route you'd like to redirect to
-    return render_template('upload.html')
+        else:
+            flash('Invalid file or no file selected', 'error')
 
-
-@app.route('/data_management')
-def data_management():
     files = os.listdir(app.config['DATA_RAW_FOLDER'])
     return render_template('data_management.html', files=files)
 
 
-@app.route('/data_modeling')
+@app.route("/data_modeling")
 def data_modeling():
-    return render_template('data_modeling.html')
+    return render_template("data_modeling.html")
+
+
+@app.route('/file_processing', methods=['POST'])
+def file_processing():
+    selected_file = request.form.get('selected_file')
+    if selected_file:
+        # Logic to process the selected file
+        # ...
+        return render_template('process_file.html', selected_file=selected_file)
+    else:
+        flash('No file selected', 'error')
+        return redirect(url_for('data_management'))
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import app
 from app import routes
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
