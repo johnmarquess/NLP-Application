ec5266bd81dc015e3e4e5b7bd5ec2795899da182
John Marquess <john@marquess.me>
2023-11-26 18:53:52 +1000

    Refactor upload method and add data management feature
    Refactored the file upload handling by combining GET and POST method in '/upload' route. Instead of having two different routes for uploading a file, now '/upload' can handle both displaying the upload form and processing the uploaded file.
    
    Also added data management feature by creating a new route '/data_management'. This allows users to manage uploaded files. This was necessary to add the ability to list, select, and process uploaded files.
    
    Added two new HTML templates for data management and data modeling pages and modified the index.html as well as upload.html to reflect these changes. Added some configurations and added a csv-editor.xml under .idea directory to better manage database CSV files.
    
    Updated base.html to include a navbar for easier navigation through the app.
    
    Overall, these changes improve the user experience by providing a clear workflow: upload data, manage data, run model.

--- app/routes.py
+++ app/routes.py
@@ -16,20 +16,31 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/upload')
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
+    if request.method == 'POST':
+        if 'datafile' not in request.files:
+            flash('No file part', 'error')
+            return redirect(request.url)
+        file = request.files['datafile']
+        if file.filename == '':
+            flash('No selected file', 'error')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            # Change this to save in the DATA_RAW_FOLDER
+            file.save(os.path.join(app.config['DATA_RAW_FOLDER'], filename))
+            flash(f'File uploaded successfully to {app.config["DATA_RAW_FOLDER"]}', 'success')
+            return redirect(url_for('index'))  # Or any other route you'd like to redirect to
     return render_template('upload.html')
 
 
-@app.route('/upload', methods=['POST'])
-def upload_file_post():
-    if 'datafile' not in request.files:
-        return redirect(request.url)
-    file = request.files['datafile']
-    if file.filename == '':
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        flash('File uploaded successfully!', 'success')
-        return redirect(url_for('index'))
+@app.route('/data_management')
+def data_management():
+    files = os.listdir(app.config['DATA_RAW_FOLDER'])
+    return render_template('data_management.html', files=files)
+
+
+@app.route('/data_modeling')
+def data_modeling():
+    return render_template('data_modeling.html')
