fbb47b1874365840eae9a37bba8eb104c0eec1a4
John Marquess <john@marquess.me>
2023-11-27 08:49:47 +1000

    Update app base title and UI strings for clarity
    The commit refreshes the base title in 'base.html' to 'NLP Modeling Tool' for better relevance to the application's purpose, and also updates link and button texts in 'view_worksheet.html' and 'view_file_contents.html' for clarity. Missing values representation is changed to a ratio in 'view_worksheet.html', and the sample size for file contents view is now reduced to 5 rows for better manageability. The unnecessary CSV file name user input section has been removed from 'view_worksheet.html' as the processed_file_name has been standardized. Changes in 'routes.py' reflect the corresponding updates in the templates as well as optimize the file processing flow. This commit aligns the app UI better with the added excel support and the enhanced data processing workflow.

--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,17 @@ def allowed_file(filename):
     )
 
 
+def get_column_info(df):
+    column_info = []
+    for column in df.columns:
+        first_item = df[column].iloc[0] if not df[column].empty else 'N/A'
+        missing_values = df[column].isna().sum()
+        total_values = len(df[column])
+        column_info.append(
+            {'name': column, 'first_item': first_item, 'missing_ratio': f"{missing_values}:{total_values}"})
+    return column_info
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -46,35 +57,23 @@ def upload_file():
 @app.route('/select_columns', methods=['GET', 'POST'])
 def select_columns():
     selected_file = request.args.get('selected_file') if request.method == 'GET' else request.form.get('selected_file')
-
-    if not selected_file:
-        flash('No file selected', 'error')
-        return redirect(url_for('data_management'))
-
     file_path = os.path.join(app.config['DATA_RAW_FOLDER'], selected_file)
 
-    # Check the file extension and process accordingly
-    if selected_file.endswith('.csv'):
-        try:
+    try:
+        if selected_file.endswith('.csv'):
             df = pd.read_csv(file_path)
-            columns = df.columns.tolist()
-            return render_template('select_columns.html', columns=columns, selected_file=selected_file, file_type='csv')
-        except Exception as e:
-            flash(f'Error reading file: {e}', 'error')
+        elif selected_file.endswith(('.xls', '.xlsx')):
+            df = pd.read_excel(file_path)
+        else:
+            flash('Unsupported file format', 'error')
             return redirect(url_for('data_management'))
 
-    elif selected_file.endswith(('.xls', '.xlsx')):
-        try:
-            xl = pd.ExcelFile(file_path)
-            sheets = xl.sheet_names
-            return render_template('select_worksheet.html', sheets=sheets, selected_file=selected_file,
-                                   file_type='excel')
-        except Exception as e:
-            flash(f'Error reading file: {e}', 'error')
-            return redirect(url_for('data_management'))
+        column_info = get_column_info(df)
+        return render_template('view_worksheet.html', column_info=column_info, selected_file=selected_file)
 
-    else:
-        flash('Unsupported file format', 'error')
+    except Exception as e:
+        flash(f'Error reading file: {e}', 'error')
+        return redirect(url_for('data_management'))
 
 
 @app.route("/data_modeling")
@@ -104,7 +103,7 @@ def view_file_contents():
     try:
         data = pd.read_csv(file_path, usecols=selected_columns)
         total_rows = len(data)
-        data_sample = data.sample(n=10)  # Or any other logic for sampling
+        data_sample = data.sample(n=5)  # Or any other logic for sampling
     except Exception as e:
         flash(f'Error loading file: {e}', 'error')
         return redirect(url_for('select_columns', selected_file=selected_file))
@@ -157,20 +156,22 @@ def save_processed_data():
 def process_selected_columns():
     selected_columns = request.form.getlist('columns')
     selected_file = request.form.get('selected_file')
-    selected_worksheet = request.form.get('selected_worksheet')
-    user_defined_name = request.form.get('csv_file_name')
     file_path = os.path.join(app.config['DATA_RAW_FOLDER'], selected_file)
 
     try:
-        df = pd.read_excel(file_path, sheet_name=selected_worksheet, usecols=selected_columns)
+        if selected_file.endswith('.csv'):
+            df = pd.read_csv(file_path, usecols=selected_columns)
+        elif selected_file.endswith(('.xls', '.xlsx')):
+            df = pd.read_excel(file_path, usecols=selected_columns)
+        else:
+            flash('Unsupported file format', 'error')
+            return redirect(url_for('data_management'))
 
-        # Construct the CSV file name
-        csv_file_name = f"{user_defined_name}.csv" if user_defined_name else f"processed_{os.path.splitext(selected_file)[0]}.csv"
-        processed_file_path = os.path.join(app.config['DATA_PROCESSED_FOLDER'], csv_file_name)
-
-        # Save to CSV
+        processed_file_name = f"processed_{os.path.splitext(selected_file)[0]}.csv"
+        processed_file_path = os.path.join(app.config['DATA_PROCESSED_FOLDER'], processed_file_name)
         df.to_csv(processed_file_path, index=False)
-        flash(f'File saved successfully as {csv_file_name}', 'success')
+        flash(f'File saved successfully as {processed_file_name}', 'success')
+
     except Exception as e:
         flash(f'Error processing file: {e}', 'error')
         return redirect(url_for('select_columns', selected_file=selected_file))
