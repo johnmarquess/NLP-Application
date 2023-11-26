6731414b7537e15c4ab4893e9116038a78f9c62f
John Marquess <john@marquess.me>
2023-11-26 17:11:03 +1000

    Implement file upload feature in Flask app
    Implemented a file upload feature in the Flask application by adding new routes and templates to handle the file upload process, including index, upload, and post-upload pages. The selected file is checked to ensure it's in the allowed extensions list before being saved to the UPLOAD_FOLDER. Confirmation message for successful upload is flashed. In addition, necessary imports were added and necessary configurations updated.
    
    Requirements.txt was updated to include Flask_Uploads and Werkzeug.
    
    This feature allows users to upload a file, which is essential for an app that requires file input for processing. This also increases usability as it simplifies the data submission process for the users.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,35 @@
+import os
+
+from flask import render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+
 from app import app
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+
+@app.route('/upload')
+def upload_file():
+    return render_template('upload.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file_post():
+    if 'datafile' not in request.files:
+        return redirect(request.url)
+    file = request.files['datafile']
+    if file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('File uploaded successfully!', 'success')
+        return redirect(url_for('index'))
