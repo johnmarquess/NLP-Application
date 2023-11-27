d42182a43f5254cecf7dcacd0df16c65a082b8fa
John Marquess <john@marquess.me>
2023-11-28 09:38:39 +1000

    Add file selection, worksheet selection, and CSV saving routes to app
    Introduced functionalities for file and worksheet selection in data_management.html and routes.py files. Users can now select a file from the list of raw data files and the details of the selected file (sheets, columns, etc.) are displayed. Similar functionality for worksheet selection within the selected file has also been added. Additionally, included a feature to save selected columns from a selected worksheet as a new CSV file. These changes will increase flexibility and improve data management by enabling users to narrow down and specifically select the data they require from the uploaded files.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
+import os
+
+import pandas as pd
+from flask import current_app
 from flask import render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
+
 from app.forms import FileUploadForm
-import os
+
+
+def get_saved_files():
+    data_saved_path = os.path.join(current_app.root_path, current_app.config['DATA_SAVED'])
+    return os.listdir(data_saved_path)
 
 
 def register_routes(app):
@@ -32,6 +41,60 @@ def register_routes(app):
             return redirect(url_for('data_management'))
 
         return render_template('data_management.html', form=form, raw_files=raw_files, saved_files=saved_files)
+
+    @app.route('/select-file', methods=['POST'])
+    def select_file():
+        form = FileUploadForm()  # Assuming this is your form class
+        selected_file = request.form['selected_file']
+        file_path = os.path.join('app', app.config['DATA_RAW'], selected_file)
+
+        xls = pd.ExcelFile(file_path)
+        sheets = xls.sheet_names
+        saved_files = get_saved_files()
+        return render_template('data_management.html', form=form, worksheets=sheets, saved_files=saved_files,
+                               selected_file=selected_file)
+
+    @app.route('/select-worksheet', methods=['POST'])
+    def select_worksheet():
+        form = FileUploadForm()  # Create an instance of your form class
+        selected_file = request.form['selected_file']
+        selected_sheet = request.form['selected_sheet']
+        file_path = os.path.join('app', app.config['DATA_RAW'], selected_file)
+
+        # Read the worksheet
+        df = pd.read_excel(file_path, sheet_name=selected_sheet)
+        columns = df.columns.tolist()
+        saved_files = get_saved_files()
+        column_info = [(col, df[col].iloc[0] if not df[col].empty else None, df[col].isna().sum()) for col in columns]
+
+        # Pass the form object along with other data to the template
+        return render_template('worksheet_display.html', column_info=column_info, saved_files=saved_files,
+                               selected_file=selected_file, selected_sheet=selected_sheet, total_rows=len(df))
+
+    @app.route('/save-csv', methods=['POST'])
+    def save_csv():
+        selected_file = request.form['selected_file']
+        selected_sheet = request.form['selected_sheet']
+        selected_columns = request.form.getlist('selected_columns')
+        csv_name = request.form['csv_name'] + '.csv'
+        file_path = os.path.join('app', app.config['DATA_RAW'], selected_file)
+        save_path = os.path.join('app', app.config['DATA_SAVED'], csv_name)
+
+        try:
+            # Read the selected worksheet
+            df = pd.read_excel(file_path, sheet_name=selected_sheet)
+
+            # Select the specified columns
+            df_selected = df[selected_columns]
+
+            # Save the DataFrame as a CSV file
+            df_selected.to_csv(save_path, index=False)
+
+            flash('CSV saved successfully!', 'success')
+        except Exception as e:
+            flash(f'Error saving CSV: {e}', 'danger')
+
+        return redirect(url_for('data_management'))
 
     @app.route('/data-modeling')
     def data_modeling():
