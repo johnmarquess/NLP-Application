a3d49516df3b8f488f637a6d72d247e57005da64
John Marquess <john@marquess.me>
2023-11-27 14:32:30 +1000

    Index page, navbar, and links. Base.html imports for pages set up.

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,17 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
-# Add other routes here
+
+@main.route('/data-management')
+def data_management():
+    return render_template('data_management.html')  # Assumes you have a template for this
+
+
+@main.route('/data-modeling')
+def data_modeling():
+    return render_template('data_modeling.html')  # Assumes you have a template for this
+
+
+@main.route('/reporting')
+def reporting():
+    return render_template('reporting.html')  # Assumes you have a template for this
