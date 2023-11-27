ad5db18d1a34ef59e41abd5aadadd78cf75a98b2
John Marquess <john@marquess.me>
2023-11-27 15:35:53 +1000

    Need to fix the fil upload functionality before anything else

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from config import config_by_name
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.secret_key = 'your_secret_key_here'
     app.config.from_object(config_by_name[config_name])
 
     from .routes import main
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,10 @@
-from flask import Blueprint, render_template
+import os
+
+from flask import Blueprint, render_template, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+
+from .forms import UploadForm
+from .utils.file_handling import list_files
 
 main = Blueprint('main', __name__)
 
@@ -8,9 +14,26 @@ def index():
     return render_template('index.html')
 
 
-@main.route('/data-management')
+@main.route('/data-management', methods=['GET', 'POST'])
 def data_management():
-    return render_template('data_management.html')  # Assumes you have a template for this
+    form = UploadForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        filename = secure_filename(file.filename)
+        save_path = os.path.join('data/data_raw', filename)
+
+        try:
+            file.save(save_path)
+            flash(f'File {filename} has been successfully saved.', 'success')
+        except Exception as e:
+            flash(f'An error occurred while saving the file: {e}', 'danger')
+
+        return redirect(url_for('main.data_management'))
+
+    raw_files = list_files('data/data_raw')
+    saved_files = list_files('data/data_saved')
+
+    return render_template('data_management.html', form=form, raw_files=raw_files, saved_files=saved_files)
 
 
 @main.route('/data-modeling')
