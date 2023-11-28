06216f80dde72a81c81c0cf89591e992854a0896
John Marquess <john@marquess.me>
2023-11-28 13:25:48 +1000

    This is working at this point. Now to add upgrade forms to WTF

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,27 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import SubmitField
+from wtforms import SubmitField, RadioField, SelectField
+from wtforms.validators import DataRequired
 
 
 class FileUploadForm(FlaskForm):
     file = FileField('Choose File', validators=[
-        FileAllowed(['csv', 'xls', 'xlsx', 'pickle', 'parquet'], 'Only specific file types allowed!')
+        DataRequired(message='No file selected!'),
+        FileAllowed(['xls', 'xlsx'], 'Only .xls and .xlsx files can be uploaded here!')
     ])
     submit = SubmitField('Upload')
+
+
+class WorksheetSelectionForm(FlaskForm):
+    worksheet = SelectField('Select Worksheet', choices=[])
+    submit = SubmitField('View Columns')
+
+
+class SpacyModelForm(FlaskForm):
+    model = RadioField('Model', choices=[
+        ('en-core-web-sm', 'en-core-web-sm'),
+        ('en-core-web-md', 'en-core-web-md'),
+        ('en-core-web-lg', 'en-core-web-lg'),
+        ('en-blank', 'en-blank')
+    ])
+    submit = SubmitField('Load Model')
