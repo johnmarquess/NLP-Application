e052aa4f3136874bfd3fdf32540a1241cba445c1
John Marquess <john@marquess.me>
2023-12-14 22:28:52 +1000

    Update model manager for handling entity addition from CSV
    The model manager has been updated to handle the addition of entities from a CSV file to a given model. The update includes changes for a concise model loading process and a new function to add entities. This allows for entity addition to a loaded model from a CSV file, where the CSV file should contain 'input' and 'entity' columns. HTML template has also been updated with an alert section to display the message generated after entity addition.

--- app/blueprints/model_manager/routes.py
+++ app/blueprints/model_manager/routes.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Blueprint, session, redirect, url_for, current_app, request
+from flask import Blueprint, session, redirect, url_for, current_app
 from flask import flash, render_template
 
 from app.modules.file_management import FileManagement
@@ -18,34 +18,32 @@ def model_manager():
     file_manager_instance = FileManagement()
     reference_form = ReferenceFileForm()
     table_html = None
-
-    # Check if a model is loaded
-    model_loaded = 'spacy_model_name' in session or 'custom_model_name' in session
-
-    if selection_form.validate_on_submit():
-        model_type = selection_form.model_type.data
-        model_identifier = ""
-
-        if model_type == 'spacy_core':
-            model_identifier = selection_form.spacy_model.data
-        elif model_type == 'custom':
-            model_identifier = selection_form.custom_model.data
-
-        model_mgr = ModelManager()
-        load_message, model = model_mgr.load_model(model_type, model_identifier)
-
-        if 'Error loading model' in load_message:
-            flash(load_message, 'error')
-        else:
-            flash(load_message, 'success')
+    update_message = None
 
     # Populate choices for reference files
     reference_files = file_manager_instance.list_files('reference')
     reference_form.reference_file.choices = [(f, f) for f in reference_files]
 
+    model_loaded = 'spacy_model_name' in session or 'custom_model_name' in session
+
+    if selection_form.validate_on_submit():
+        model_type = selection_form.model_type.data
+        model_identifier = selection_form.spacy_model.data if model_type == 'spacy_core' else selection_form.custom_model.data
+
+        model_mgr = ModelManager()
+        load_message, model = model_mgr.load_model(model_type, model_identifier)
+        flash(load_message, 'error' if 'Error loading model' in load_message else 'success')
+
+    # Handling the reference file form submission
     if reference_form.validate_on_submit():
         selected_reference_file = reference_form.reference_file.data
         file_path = os.path.join(current_app.config['REFERENCE_DATA_DIR'], selected_reference_file)
+
+        model_mgr = ModelManager()
+        update_message = model_mgr.add_entities_from_csv(file_path)
+        flash(update_message)
+
+        # Optionally, display the contents of the file
         table_html = file_manager_instance.view_csv_contents(file_path)
 
     return render_template('model_management.html',
@@ -53,7 +51,8 @@ def model_manager():
                            save_form=save_form,
                            reference_form=reference_form,
                            table_html=table_html,
-                           model_loaded=model_loaded)
+                           model_loaded=model_loaded,
+                           update_message=update_message)
 
 
 @model_manager_bp.route('/save-model', methods=['POST'])
@@ -75,6 +74,3 @@ def save_model():
             flash(save_message, 'success')
 
     return redirect(url_for('model_manager.model_manager'))
-
-
-
